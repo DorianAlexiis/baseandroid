66c88e98b9d50836b78569ab93169be16f5a5636
Dorian Rios <dorianalexiis@gmail.com>
2018-07-08 17:57:09 -0300

    add init fabric

--- base/src/main/java/com/android/dars/base/BaseApp.kt
+++ base/src/main/java/com/android/dars/base/BaseApp.kt
@@ -1,9 +1,19 @@
 package com.android.dars.base
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 
 open class BaseApp : Application(){
+
     override fun onCreate() {
         super.onCreate()
+        setUseFabric()
+    }
+
+    open fun setUseFabric(enable:Boolean = false){
+        Fabric.with(this, Crashlytics.Builder().core(CrashlyticsCore.Builder()
+                .disabled(!enable).build()).build(), Crashlytics())
     }
 }
